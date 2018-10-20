05660fa908836dbf6794a69e39557aed8e8227e1
Paolo Calvaresi <paolo.calvaresi.v@gmail.com>
2018-10-20 10:02:21 +0200

    aggiunto livereload

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var path = require('path');
 
-//connessione al database
+//CONFIGURAZIONE PER LA CONNESSIONE AL DATABASE
 require('./config/db.js');
-
 
 // REQUIRE DELLE ROTTE DELLA MIA APPLICAZIONE
 require('./routes/routes.js')(express, app);
 
+//LIVERELOAD
+var livereload = require('livereload');
+var server = livereload.createServer();
+server.watch(path.join(__dirname, "..", "public"));
+
+//START DEL SERVER
 app.listen(3000, function () {
     console.log("http://localhost:3000")
 })
